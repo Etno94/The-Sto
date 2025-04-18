046c8e643303d4322d74c6539387c126b500f3de
NaN.ssel <romero.juan.ignacio.1294@gmail.com>
2025-04-17 22:04:09 -0300

    moved to folder src

--- engine/data/storage.data.js
+++
@@ -1,9 +0,0 @@
-export const STORAGE_UPGRADES = {
-    maxStorageUpgradeCurrentLevel: 0,
-    initialMaxStorage: 3,
-    maxStorageUpgrade: 1,
-    maxStorageUpgradeMaxLevel: 7,
-    maxStorageUpgradeCostFormula: (maxStorageUpgradeCurrentLevel) => 2 ^ maxStorageUpgradeCurrentLevel,
-    currentMaxStorage: (initialMaxStorage, maxStorageUpgradeCurrentLevel, maxStorageUpgrade) => initialMaxStorage + (maxStorageUpgradeCurrentLevel * maxStorageUpgrade),
-}
-
