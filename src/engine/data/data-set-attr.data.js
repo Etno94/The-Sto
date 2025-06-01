db0cc4d68dc5e9dc25ab0c9b661545a672f1a2f2
NaN.ssel <romero.juan.ignacio.1294@gmail.com>
2025-06-01 11:45:40 -0300

    data set data

---
+++ src/engine/data/data-set-attr.data.js
@@ -0,0 +1,17 @@
+/**
+ * @type {Object<string, string>}
+ */
+export const DATA_SET_ATTRs = {
+    type: 'type',
+    id: 'id',
+    name: 'name',
+    value: 'value',
+    class: 'class',
+    classes: 'classes',
+    pointType: 'point-type',
+    pointId: 'point-id',
+    pointName: 'point-name',
+    pointValue: 'point-value',
+    generatorName: 'generator-name',
+    generatorId: 'generator-id'
+};
