cb4edfdaa0c4fe5cfe4b12875a0fd42c18a654b6
NaN.ssel <romero.juan.ignacio.1294@gmail.com>
2025-06-02 22:52:24 -0300

    new types added

---
+++ src/engine/types/generator.type.js
@@ -0,0 +1,8 @@
+/**
+ * @typedef {Object} SaveGenerator
+ * @property {string} name
+ * @property {boolean} hinted
+ * @property {boolean} canBuild
+ * @property {number} [progress=0]
+ * @property {boolean} built
+ */
---
+++ src/engine/types/save.type.js
@@ -0,0 +1,9 @@
+/**
+ * @typedef { Object } SaveType
+ * @property { Collection } points
+ * @property { string[] } points_order
+ * @property { SaveStorage } storage
+ * @property { number } maxStorageUpgradeCurrentLevel
+ * @property { number } maxStorage
+ * @property { SaveGenerator[] } generators
+ */
