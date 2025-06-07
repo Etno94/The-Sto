1abd5c1d6f7ceb883a32f5abd0441cf25ed0bf0c
NaN.ssel <romero.juan.ignacio.1294@gmail.com>
2025-06-07 15:52:49 -0300

    animate updated
    added Asserts

--- src/engine/views/helpers/animate.js
+++ src/engine/views/helpers/animate.js
@@ -1,4 +1,6 @@
 import Utils from '../../utils/utils.js';
+import Asserts from '../../utils/asserts.js';
+
 import UIHelper from './ui-helper.js';
 
 export default class Animate {
@@ -10,6 +12,9 @@ export default class Animate {
      * @param {AnimationItem} animation 
      */
     static async timedOut(element, animation) {
+        Asserts.htmlElement(element);
+        Asserts.object(animation, 'animation');
+
         UIHelper.toggleClass(element, animation.classes);
         await Utils.delay(animation.timer);
         UIHelper.toggleClass(element, animation.classes);
@@ -19,6 +24,8 @@ export default class Animate {
      * @param {HTMLElement} element 
      */
     static widthIn(element) {
+        Asserts.htmlElement(element);
+
         if (!UIHelper.containsClass(element, 'no-width')) return;
         element.offsetWidth; // We force a layout calculation to ensure the class is applied before the next frame
         requestAnimationFrame(() => {
@@ -30,7 +37,9 @@ export default class Animate {
      * @param {HTMLElement} element 
      */
     static widthOut(element) {
-        if (UIHelper.containsClass(element, 'no-width')) return;
-        UIHelper.addClass(element, 'no-width');
+        Asserts.htmlElement(element);
+
+        if (!UIHelper.containsClass(element, 'no-width'))
+            UIHelper.addClass(element, 'no-width');
     }
 }
