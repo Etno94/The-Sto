e0892b3e6b3d88457da6e8e03a360cfe5002d324
NaN.ssel <romero.juan.ignacio.1294@gmail.com>
2025-07-03 13:31:24 -0300

    --partial commit - ui registry service

--- src/engine/views/ui-registries/ui-registry.service.js
+++ src/engine/views/ui-registries/ui-registry.service.js
@@ -1,4 +1,31 @@
+import {DataManager} from "../../systems/managers-index.js";
+
 class UIRegistryService {
+
+    #dataGeneratorRegistry;
+
+    constructor() {
+        this.#setData();
+    }
+
+    #setData() {
+        this.#dataGeneratorRegistry = DataManager.getDataGeneratorRegistry();
+    }
+    /**
+     * @param {string} generatorName 
+     * @param {HTMLElement[]} pointChanceElements 
+     */
+    registerGeneratorPointChanceElements(generatorName, pointChanceElements) {
+        let newIndex = 0;
+        const lastRegistryKey = GenUIReg.checkLastRegister(generatorName, this.#dataGeneratorRegistry.category.statusItems);
+        if (lastRegistryKey) newIndex = Number(lastRegistryKey.split('#')[1]) + 1;
+
+        pointChanceElements.forEach((pointChanceElement) => {
+            GenUIReg.register(generatorName, this.#dataGeneratorRegistry.category.statusItems, 
+                `${this.#dataGeneratorRegistry.itemPrefixes.pointChance}#${newIndex}`, pointChanceElement);
+            newIndex++;
+        });
+    }
 
 }
 export const UIRegService = new UIRegistryService();
