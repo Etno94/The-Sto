441a1ca67610b3438f231b2ef21df73c063971f2
NaN.ssel <romero.juan.ignacio.1294@gmail.com>
2026-05-19 13:28:55 -0300

    balance

--- src/engine/data/generators.data.js
+++ src/engine/data/generators.data.js
@@ -36,8 +36,8 @@ export const CLICK_GENERATOR = {
             {
                 type: POINT_TYPES.point,
                 baseChance: 80,
-                updateChanceOnSuccess: -0.2,
-                updateChanceOnFail: 1,
+                updateChanceOnSuccess: -2,
+                updateChanceOnFail: 3,
                 startingGuaranteedChanceTries: 3
             }
         ]
